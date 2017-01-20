82a38765050652fca90212272322e99548e7b0d8
Zhigang Song <1005411480a@gmail.com>
2017-01-20 13:40:13 +0800

    libmysqlclient test code

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 
-CFLAGS += -DDEBUG -g -Wall
-LDFLAGS += -lssl -lcrypto
+CFLAGS += -DDEBUG -g -Wall `mysql_config --cflags`
+LDFLAGS += -lssl -lcrypto `mysql_config --libs`
 
 client: client.o handshake.o packet.o tools.o query.o binary_type.o binlog.o rows_event.o decimal.o debug.o
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^
--- test.c
+++ test.c
@@ -5,5 +5,35 @@ int main(int argc, char **argv)
 {
     printf("MySQL client version: %s\n", mysql_get_client_info());
 
+    MYSQL *con = mysql_init(NULL);
+    if (con == NULL) {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        exit(2);
+    }
+
+    if (mysql_real_connect(con, "127.0.0.1", "root", "1111", NULL, 0, NULL, 0) == NULL) {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }
+
+    if (mysql_query(con, "CREATE DATABASE testdb")) {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }
+
+    int fd = con->net.fd;
+    char buf[1024];
+
+    write(fd, "SHOW DATABASES;", 15);
+    read(fd, &buf, 1024);
+
+    printf("%s\n", buf);
+
+
+    mysql_close(con);
+    exit(0);
+
     exit(0);
 }
